ffe97b397919d5445fb20ea9ddc7ef90e907114b
shijumylapra <shijumylapra@gmail.com>
2024-08-25 05:56:31 -0700

    Added Scenario Outline and Runner file

--- src/test/java/stepDefs/Hooks.java
+++ src/test/java/stepDefs/Hooks.java
@@ -9,26 +9,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
-public class Hooks
-{
-    public static ExtentReports report = new ExtentReports("reports.html",false) ;
+public class Hooks {
+    public static ExtentReports report = new ExtentReports("reports.html", false);
     public static ExtentTest test;
     public static WebDriver wDriver;
 
     @Before
-    public void SetUp(Scenario scenario)
-    {
-
+    public void SetUp(Scenario scenario) {
         wDriver = new ChromeDriver();
         test = report.startTest(scenario.getName());
     }
 
     @After
-    public void TearDown()
-    {
+    public void TearDown() {
         report.endTest(test);
         report.close();
-       // wDriver.quit();
+        wDriver.quit();
 
     }
 
